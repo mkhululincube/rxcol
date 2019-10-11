0735e654f01213093b1a9cc12d9f86d947dde633
mkhululi ncube <mkhululincube@mkhululis-MacBook-Air.local>
2019-10-11 23:51:49 +0400

    ltd

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 
 //Bootstrap
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Footer = () => {
-
-    return (
-        <footer>
-            <div><br/>
-                <Navbar bg="light" variant="light" sticky="bottom"><Navbar  />
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#">© Copyright 2019 Leadermold Private Limited</Nav.Link>
-                        <Nav.Link href="#">Contact us</Nav.Link>
-                    </Nav>
-                </Navbar>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <div>
+        <br />
+        <Navbar bg="light" variant="light" sticky="bottom">
+          <Navbar />
+          <Nav className="mr-auto">
+            <Nav.Link href="#">
+              © Copyright 2019 Leadermold Private Ltd
+            </Nav.Link>
+            <Nav.Link href="#">Contact us</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
